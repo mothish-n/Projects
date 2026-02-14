e81a3621373df54ba623b3ce2e26396e09282979
N Mothish <mothishmothi55@gmail.com>
2026-02-14 22:23:13 +0530

    SCORE TRACKER
    NA

---
+++ a2.js
@@ -0,0 +1,30 @@
+let h=0;
+let g=0;
+
+let he=document.getElementById("hid")
+let ge=document.getElementById("gid")
+
+function h1(){
+    h+=1;
+    he.textContent=h;
+}
+function h2(){
+    h+=2;
+    he.textContent=h;
+}
+function h3(){
+    h+=3;
+    he.textContent=h;
+}
+function g1(){
+    g+=1;
+    ge.textContent=g;
+}
+function g2(){
+    g+=2;
+    ge.textContent=g;
+}
+function g3(){
+    g+=3;
+    ge.textContent=g;
+}
